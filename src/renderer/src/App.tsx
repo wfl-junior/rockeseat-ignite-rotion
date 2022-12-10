b2eb9765077c9c8f721dcfc68f4ff243af361166
Wallace Júnior <wflj1997@gmail.com>
2022-12-10 15:36:17 -0300

    Setup do TailwindCSS

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,3 +1,5 @@
 export const App: React.FC = () => (
-  <h1 style={{ fontFamily: "Inter, sans-serif" }}>Hello World</h1>
+  <div className="w-screen h-screen flex items-center justify-center bg-zinc-900">
+    <h1 className="text-zinc-100 text-4xl font-semibold">Hello World</h1>
+  </div>
 );
