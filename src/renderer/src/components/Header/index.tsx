22ca3184c7840f008cd5b4050ef3d11cb15b6c3d
Wallace Júnior <wflj1997@gmail.com>
2022-12-11 12:14:50 -0300

    Remoção do documento

--- src/renderer/src/components/Header/index.tsx
+++ src/renderer/src/components/Header/index.tsx
@@ -1,7 +1,10 @@
 import * as Collapsible from "@radix-ui/react-collapsible";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import clsx from "clsx";
 import { CaretDoubleRight, Code, TrashSimple } from "phosphor-react";
 import { Fragment } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import type { Document } from "~/shared/types/ipc";
 import * as Breadcrumbs from "./Breadcrumbs";
 
 interface HeaderProps {
@@ -9,13 +12,39 @@ interface HeaderProps {
 }
 
 export const Header: React.FC<HeaderProps> = ({ isSidebarOpen }) => {
+  const { id } = useParams();
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const { mutateAsync: deleteDocument, isLoading: isDeletingDocument } =
+    useMutation(
+      async () => {
+        await window.api.documents.delete({ id: id! });
+      },
+      {
+        onSuccess: () => {
+          queryClient.setQueryData<Document[]>(
+            ["documents"],
+            (documents = []) => {
+              return documents.filter(document => document.id !== id);
+            },
+          );
+
+          navigate("/");
+        },
+      },
+    );
+
   const isMacOS = process.platform === "darwin";
+
+  async function handleDeleteDocument() {
+    await deleteDocument();
+  }
 
   return (
     <div
       id="header"
       className={clsx(
-        "border-rotion-600 duration-250 region-drag flex items-center gap-4 border-b py-[1.125rem] px-6 leading-tight transition-all",
+        "border-rotion-600 duration-250 region-drag flex h-[57px] items-center gap-4 border-b py-[1.125rem] px-6 leading-tight transition-all",
         isSidebarOpen ? "w-[calc(100vw-240px)]" : "w-screen",
         {
           "pl-24": !isSidebarOpen && isMacOS,
@@ -31,27 +60,33 @@ export const Header: React.FC<HeaderProps> = ({ isSidebarOpen }) => {
         <CaretDoubleRight className="h-4 w-4" />
       </Collapsible.Trigger>
 
-      <Fragment>
-        <Breadcrumbs.Root>
-          <Breadcrumbs.Item>
-            <Code weight="bold" className="h-4 w-4 text-pink-500" />
-            Estrutura técnica
-          </Breadcrumbs.Item>
-          <Breadcrumbs.Separator />
-          <Breadcrumbs.HiddenItems />
-          <Breadcrumbs.Separator />
-          <Breadcrumbs.Item>Back-end</Breadcrumbs.Item>
-          <Breadcrumbs.Separator />
-          <Breadcrumbs.Item isActive>Untitled</Breadcrumbs.Item>
-        </Breadcrumbs.Root>
+      {Boolean(id) && (
+        <Fragment>
+          <Breadcrumbs.Root>
+            <Breadcrumbs.Item>
+              <Code weight="bold" className="h-4 w-4 text-pink-500" />
+              Estrutura técnica
+            </Breadcrumbs.Item>
+            <Breadcrumbs.Separator />
+            <Breadcrumbs.HiddenItems />
+            <Breadcrumbs.Separator />
+            <Breadcrumbs.Item>Back-end</Breadcrumbs.Item>
+            <Breadcrumbs.Separator />
+            <Breadcrumbs.Item isActive>Untitled</Breadcrumbs.Item>
+          </Breadcrumbs.Root>
 
-        <div className="region-no-drag inline-flex">
-          <button className="text-rotion-100 hover:text-rotion-50 inline-flex items-center gap-1 text-sm">
-            <TrashSimple className="h-4 w-4" />
-            Apagar
-          </button>
-        </div>
-      </Fragment>
+          <div className="region-no-drag inline-flex">
+            <button
+              onClick={handleDeleteDocument}
+              disabled={isDeletingDocument}
+              className="text-rotion-100 hover:text-rotion-50 inline-flex items-center gap-1 text-sm disabled:cursor-not-allowed disabled:opacity-60"
+            >
+              <TrashSimple className="h-4 w-4" />
+              Apagar
+            </button>
+          </div>
+        </Fragment>
+      )}
     </div>
   );
 };
